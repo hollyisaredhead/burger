df5092fcc36efdeebd7c1d22c0c73498331e7468
Holly Phelps <Hollyphelps1992@gmail.com>
2020-02-27 23:30:07 -0800

    Updates to config

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bacon Cheddar', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Mushroom Swiss', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Cheese', true);
